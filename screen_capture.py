6353e56c69919a099dc73162b30a23774963d53f
MartyrHartman <martyrhartman@yandex.ru>
2022-01-23 11:08:43 +0300

    23.01.2022 homePC

--- screen_capture.py
+++ screen_capture.py
@@ -5,32 +5,18 @@ import pyautogui
 
 class Capturer:
 
-    def __init__(self, width, height, fps, resolution='1080p'):
-        if resolution == '1080p':
-            self.monitor = {
-                'top': 1080 - height,
-                'left': 1920 - width,
-                'width': width,
-                'height': height
-            }
+    def __init__(self, width, height, fps):
 
-        elif resolution == '1440p':
-            self.monitor = {
-                'top': 1440 - height,
-                'left': 2560 - width,
-                'width': width,
-                'height': height
-            }
-
-        else:
-            raise ValueError('Unsupported monitor resolution')
+        self.width = width
+        self.height = height
+        self.fps = fps
         self.vid = cv2.VideoWriter(
             'output.avi',
             cv2.VideoWriter_fourcc(*'XVID'),
-            fps=fps,
+            fps=self.fps,
             frameSize=(
-                self.monitor['width'],
-                self.monitor['height']
+                self.width,
+                self.height
             )
         )
 
@@ -47,7 +33,8 @@ class Capturer:
 if __name__ == '__main__':
     w = int(input("Width = "))
     h = int(input("Height = "))
-    capt = Capturer(w, h, 20.0)
+    fps = float(input("FPS = "))
+    capt = Capturer(w, h, fps=fps)
     try:
         print('TO EXIT PRESS Ctrl+C')
         capt.mainloop()
